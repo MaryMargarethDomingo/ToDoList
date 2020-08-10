0a54562062357904613ba5e02567d8427bf109d4
MaryMargarethDomingo <domingo.marymargareth@gmail.com>
2020-08-10 16:47:30 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+  const [toDoList, setToDoList]=useState([
+    {text:"Study React"},
+    {text:"Study React"}
+  ]);
 
+  //set input value
+  const [value, setValue]=useState(['']);
+ 
+
+  //Form Submission
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    addToDo(value);
+    setValue('');
+  };
+  
+  //Add and Update Todo List
+  const addToDo =(text)=>{
+    const updateToDoList=[...toDoList, {text}];
+    setToDoList(updateToDoList);
+  }
+  return(
+    <div>
+      <h1>To Do List</h1>
+      <div className= 'toDoList'>
+      {
+        toDoList.map((todo, index)=>(
+         
+            <p>{todo.text}</p>
+         
+        ))
+      }
+      </div>
+      <div className='todoInput'>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Things to do....."
+        value={value}
+        onChange={addText=>setValue(addText.target.value)}
+        />
+        <button type='submit'>Add</button>
+      </form>
+      </div>
+    </div>
+  )
+}
 export default App;
