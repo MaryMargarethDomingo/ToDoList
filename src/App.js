25597d94b845806ee729f53c498e18467ebbca93
jersrej <jerson.conmigo@gmail.com>
2020-08-10 17:07:07 +0800

    Add delete function
    Add delete function

--- src/App.js
+++ src/App.js
@@ -1,50 +1,66 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import uuid from "react-uuid";
+import "./App.css";
 
-function App(){
-  const [toDoList, setToDoList]=useState([
-    {text:"Study React"},
-    {text:"Study React"}
-  ]);
+function App() {
+  const [toDoList, setToDoList] = useState(null);
 
   //set input value
-  const [value, setValue]=useState(['']);
- 
+  const [value, setValue] = useState([]);
 
   //Form Submission
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     addToDo(value);
-    setValue('');
+    setValue([]);
   };
-  
+
   //Add and Update Todo List
-  const addToDo =(text)=>{
-    const updateToDoList=[...toDoList, {text}];
+  const addToDo = (text) => {
+    let updateToDoList = [{ id: uuid(), text: text }];
+
+    if (toDoList !== null) {
+      updateToDoList = [...toDoList, { id: uuid(), text: text }];
+    }
+
     setToDoList(updateToDoList);
-  }
-  return(
+  };
+
+  const deleteList = (id) => {
+    const todoFilter = toDoList.filter((v) => v.id !== id);
+
+    setToDoList(todoFilter);
+  };
+
+  return (
     <div>
       <h1>To Do List</h1>
-      <div className= 'toDoList'>
-      {
-        toDoList.map((todo, index)=>(
-         
-            <p>{todo.text}</p>
-         
-        ))
-      }
+      <div className='toDoList'>
+        {toDoList !== null &&
+          toDoList.map((todo, index) => (
+            <div
+              style={{
+                display: "flex",
+                alignContent: "center",
+                justifyContent: "center",
+              }}
+            >
+              <p key={`id-${todo.id}`}>{todo.text}</p>
+              <button onClick={() => deleteList(todo.id)}>Delete</button>
+            </div>
+          ))}
       </div>
       <div className='todoInput'>
-      <form onSubmit={handleSubmit}>
-        <input placeholder="Things to do....."
-        value={value}
-        onChange={addText=>setValue(addText.target.value)}
-        />
-        <button type='submit'>Add</button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder='Things to do.....'
+            value={value}
+            onChange={(addText) => setValue(addText.target.value)}
+          />
+          <button type='submit'>Add</button>
+        </form>
       </div>
     </div>
-  )
+  );
 }
 export default App;
